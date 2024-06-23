71c5b07fd8ba35700b0de5967e8c759221e2252c
BakaBakaGaijin <legostaevofficial@yandex.ru>
2024-06-23 16:49:19 +0300

    Фильтрация по тегам

--- pages/feed/api/loader.ts
+++ pages/feed/api/loader.ts
@@ -1,13 +1,31 @@
-import { json } from "@remix-run/node";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import type { FetchResponse } from "openapi-fetch";
+import { promiseHash } from "remix-utils/promise";
 
 import { GET } from "shared/api";
 
-export const loader = async () => {
-  const { data: articles, error, response } = await GET("/articles");
+const throwAnyErrors = async <T, O, Media extends `${string}/${string}`>(
+  responsePromise: Promise<FetchResponse<T, O, Media>>,
+) => {
+  const { data, error, response } = await responsePromise;
 
   if (error) {
     throw json(error, { status: response.status });
   }
 
-  return json({ articles });
+  return data as NonNullable<typeof data>;
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const selectedTag = url.searchParams.get("tag") ?? undefined;
+
+  return json(
+    await promiseHash({
+      articles: throwAnyErrors(
+        GET("/articles", { params: { query: { tag: selectedTag } } }),
+      ),
+      tags: throwAnyErrors(GET("/tags")),
+    }),
+  );
 };
